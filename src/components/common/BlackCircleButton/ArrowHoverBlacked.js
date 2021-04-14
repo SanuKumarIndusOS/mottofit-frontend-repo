b27f3676bb9131791c31907e0139b694bb09ca27
Praveen M <praveen.02@doodleblue.com>
2021-04-14 19:13:44 +0530

    line fix update

--- src/components/common/BlackCircleButton/ArrowHoverBlacked.js
+++ src/components/common/BlackCircleButton/ArrowHoverBlacked.js
@@ -7,7 +7,7 @@ function ArrowHoverBlacked() {
             {/* <div className="button_text">Submit</div> */}
             <div className="arrow_container">
                 <div className="arrow_anim" id="arrow_anim">
-                    <div className="line"></div>
+                    <div className="line_btn"></div>
                     <div className="arrow right"></div>
                 </div>
                 <div className="arrow2 right" id="cube"></div>
