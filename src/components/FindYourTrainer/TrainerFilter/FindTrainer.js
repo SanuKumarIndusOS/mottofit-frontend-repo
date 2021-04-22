1d08e263a11bf618a4107ec0329744544c9684e8
Praveen M <praveen.02@doodleblue.com>
2021-04-22 17:38:22 +0530

    setlocation UI update

--- src/components/FindYourTrainer/TrainerFilter/FindTrainer.js
+++ src/components/FindYourTrainer/TrainerFilter/FindTrainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./find.sass";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -15,181 +15,199 @@ import AvailabilityIcon from "../../../assets/Home/Banner/SearchBar/Availability
 // import { Schedule } from "@material-ui/icons";
 
 const FindTrainer = () => {
-    const [selectedDate, setSelectedDate] = useState(new Date());
+  // useEffect(() => {
+  //   return SetLocation("Virtual");
+  // }, []);
 
-    var Training_vertical = [];
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
-    const [LocationState, setLocationState] = useState('Virtual')
+  var Training_vertical = [];
 
-    const [DropdownState, setDropdownState] = useState(false);
-    const [DropdownValue, setDropdownValue] = useState([]);
+  const [DropdownState, setDropdownState] = useState(false);
+  const [DropdownValue, setDropdownValue] = useState([]);
 
-    const [ddBoxingState, setddBoxingState] = useState(false);
-    const [ddPilatesState, setddPilatesState] = useState(false);
-    const [ddYogaState, setddYogaState] = useState(false);
-    const [ddHiitState, setddHiitState] = useState(false);
+  const [ddBoxingState, setddBoxingState] = useState(false);
+  const [ddPilatesState, setddPilatesState] = useState(false);
+  const [ddYogaState, setddYogaState] = useState(false);
+  const [ddHiitState, setddHiitState] = useState(false);
 
-    const onClickHandle = () => {
-        setSelectedDate(selectedDate);
-        console.log(selectedDate);
-    };
-
-    let Dropdown;
-
-    if (DropdownState) {
-        Dropdown = (
-            <div className="box_3_active">
-                <div className="dd_row_one">
-                    <div
-                        className="option"
-                        onClick={() => {
-                            setddBoxingState(!ddBoxingState);
-                            console.log(ddBoxingState, "boxing");
-                        }}
-                    >
-                        <div className="option_wapper">
-                            <img src={BoxingIcon} alt="icon" />
-                            <h2>Boxing</h2>
-                        </div>
-                    </div>
-                    <div
-                        className="option"
-                        onClick={() => {
-                            setddPilatesState(!ddPilatesState);
-                            console.log(ddPilatesState, "pilates");
-                        }}
-                    >
-                        <div className="option_wapper">
-                            <img src={PilatesIcon} alt="icon" />
-                            <h2>Pilates</h2>
-                        </div>
-                    </div>
-                </div>
-                <div className="dd_row_two">
-                    <div
-                        className="option"
-                        onClick={() => {
-                            setddHiitState(!ddHiitState);
-                            console.log(ddHiitState, "hiit");
-                        }}
-                    >
-                        <div className="option_wapper">
-                            <img src={StrengthIcon} alt="icon" />
-                            <h2>Strength & Hiit</h2>
-                        </div>
-                    </div>
-                    <div
-                        className="option"
-                        onClick={() => {
-                            setddYogaState(!ddYogaState);
-                            console.log(ddYogaState, "yoga");
-                        }}
-                    >
-                        <div className="option_wapper">
-                            <img src={YogaIcon} alt="icon" />
-                            <h2>Yoga</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    } else {
-        Dropdown = <div className="box_3_inactive"></div>;
-    }
-
-    const TriggerVerticalDropDown = () => {
-        setDropdownState(!DropdownState);
-        setDropdownValue("Boxing");
-        console.log(DropdownState);
-    };
+  const onClickHandle = () => {
+    setSelectedDate(selectedDate);
     console.log(selectedDate);
+  };
 
-    const SetLocation = (value) => {
-       console.log(value);
+  let Dropdown;
 
-       if(value === "Virtual")
-       {
-           console.log("virtual_p");
-       }else
-       {
-           console.log("pp");
-       }
+  if (DropdownState) {
+    Dropdown = (
+      <div className="box_3_active">
+        <div className="dd_row_one">
+          <div
+            className="option"
+            onClick={() => {
+              setddBoxingState(!ddBoxingState);
+              console.log(ddBoxingState, "boxing");
+            }}
+          >
+            <div className="option_wapper">
+              <img src={BoxingIcon} alt="icon" />
+              <h2>Boxing</h2>
+            </div>
+          </div>
+          <div
+            className="option"
+            onClick={() => {
+              setddPilatesState(!ddPilatesState);
+              console.log(ddPilatesState, "pilates");
+            }}
+          >
+            <div className="option_wapper">
+              <img src={PilatesIcon} alt="icon" />
+              <h2>Pilates</h2>
+            </div>
+          </div>
+        </div>
+        <div className="dd_row_two">
+          <div
+            className="option"
+            onClick={() => {
+              setddHiitState(!ddHiitState);
+              console.log(ddHiitState, "hiit");
+            }}
+          >
+            <div className="option_wapper">
+              <img src={StrengthIcon} alt="icon" />
+              <h2>Strength & Hiit</h2>
+            </div>
+          </div>
+          <div
+            className="option"
+            onClick={() => {
+              setddYogaState(!ddYogaState);
+              console.log(ddYogaState, "yoga");
+            }}
+          >
+            <div className="option_wapper">
+              <img src={YogaIcon} alt="icon" />
+              <h2>Yoga</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    Dropdown = <div className="box_3_inactive"></div>;
+  }
+
+  const TriggerVerticalDropDown = () => {
+    setDropdownState(!DropdownState);
+    setDropdownValue("Boxing");
+    console.log(DropdownState);
+  };
+  console.log(selectedDate);
+
+  const [virtualMarkup, setvirtualMarkup] = useState(
+    <p style={{ borderBottom: "3px solid #53BFD2" }}>Virtual</p>
+  );
+  const [inPersonMarkup, setinPersonMarkup] = useState(
+    <p style={{ fontWeight: "normal" }}>In Person</p>
+  );
+
+  const SetLocation = (value) => {
+    console.log(value);
+
+    if (value === "Virtual") {
+
+      setvirtualMarkup(<p style={{ borderBottom: "3px solid #53BFD2" }}>Virtual</p>)
+      setinPersonMarkup( <p style={{ fontWeight: "normal" }}>In Person</p>)
+    
+    } else {
+      setvirtualMarkup( <p style={{ fontWeight: "normal" }}>Virtual</p>)
+      setinPersonMarkup( <p style={{ borderBottom: "3px solid #53BFD2" }}>In Person</p>)
     }
+  };
 
+  const search_action = () => {
+    console.log("search action");
+    var search_query;
+  };
 
-    const search_action = () => {
-        console.log("search action");
-        var search_query;
-    };
+  return (
+    <>
+      <div className="card container" style={{ border: "none" }}>
+        <div className="card-wrapper">
+          <div className="item1">
+            <h3>Location</h3>
+            <div className="card-item">
+              <div onClick={() => SetLocation("Virtual")}>
+                {/* <p
+                  style={{
+                    borderBottom: "3px solid #53BFD2",
+                  }}
+                >
+                  Virtual
+                </p> */}
+                {virtualMarkup}
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="9"
+                height="27"
+                viewBox="0 0 9 27"
+              >
+                <text
+                  id="_"
+                  data-name="/"
+                  transform="translate(1 22)"
+                  fill="#53bfd2"
+                  fontSize="20"
+                  fontFamily="SegoeUI, Segoe UI"
+                >
+                  <tspan x="0" y="0">
+                    /
+                  </tspan>
+                </text>
+              </svg>
+              <div onClick={() => SetLocation("InPerson")}>
+                {/* <p style={{ fontWeight: "normal" }}>In Person</p> */}{" "}
+                {inPersonMarkup}
+              </div>
+            </div>
+          </div>
+          <LineBetween />
+          <div className="item2">
+            <h3>Training Vertical</h3>
+            <div className="card-item" onClick={TriggerVerticalDropDown}>
+              <img src={Weight} alt="icon" />
+              <p>Boxing</p>
+            </div>
+            {Dropdown}
+          </div>
+          <LineBetween />
 
-    return (
-        <>
-            <div className="card container" style={{ border: "none" }}>
-                <div className="card-wrapper">
-                    <div className="item1">
-                        <h3>Location</h3>
-                        <div className="card-item">
-                            
-                            <div onClick={()=> SetLocation('Virtual')}><p style={{textDecoration:"underline"}}>Virtual</p></div>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="9"
-                                height="27"
-                                viewBox="0 0 9 27"
-                            >
-                                <text
-                                    id="_"
-                                    data-name="/"
-                                    transform="translate(1 22)"
-                                    fill="#53bfd2"
-                                    fontSize="20"
-                                    fontFamily="SegoeUI, Segoe UI"
-                                >
-                                    <tspan x="0" y="0">
-                                        /
-                                    </tspan>
-                                </text>
-                            </svg>
-                            <div onClick={() => SetLocation('InPerson')}><p>In Person</p></div>
-                        </div>
-                    </div>
-                    <LineBetween />
-                    <div className="item2">
-                        <h3>Training Vertical</h3>
-                        <div
-                            className="card-item"
-                            onClick={TriggerVerticalDropDown}
-                        >
-                            <img src={Weight} alt="icon" />
-                            <p>Boxing</p>
-                        </div>
-                        {Dropdown}
-                    </div>
-                    <LineBetween />
+          <div className="item3">
+            <h3>Schedule</h3>
+            <div className="card-item">
+              <img src={SheduleIcon} alt="icon" />
+              <DatePicker
+                onChange={(date) => setSelectedDate(date)}
+                selected={selectedDate}
+                dateFormat="dd/MM/yyyy"
+                minDate={new Date()}
+                showYearDropdown
+                scrollableMonthYearDropdown
+              ></DatePicker>
 
-                    <div className="item3">
-                        <h3>Schedule</h3>
-                        <div className="card-item">
-                            <img src={SheduleIcon} alt="icon" />
-                            <DatePicker
-                                onChange={(date) => setSelectedDate(date)}
-                                selected={selectedDate}
-                                dateFormat="dd/MM/yyyy"
-                                minDate={new Date()}
-                                showYearDropdown
-                                scrollableMonthYearDropdown
-                            ></DatePicker>
+              <DropDownSVG onClick={onClickHandle} />
+            </div>
+          </div>
+          <LineBetween />
 
-                            <DropDownSVG onClick={onClickHandle} />
-                        </div>
-                    </div>
-                    <LineBetween />
-
-                    <div className="item4">
-                        <h3>Availability</h3>
-                        <div className="card-item">
-                            <img src={AvailabilityIcon} alt="icon" />
-                            {/* <select>
+          <div className="item4">
+            <h3>Availability</h3>
+            <div className="card-item">
+              <img src={AvailabilityIcon} alt="icon" />
+              {/* <select>
                 <option value="">Boxing</option>
                 <option value="">Pilates</option>
                 <option value="">Strength & hiit</option>
