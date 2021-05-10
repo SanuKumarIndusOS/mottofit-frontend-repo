c8cee4c44ed4aa251dea3ace2c1aa72f82b18d8c
Praveen M <praveen.02@doodleblue.com>
2021-05-10 18:36:39 +0530

    build update

---
+++ build/static/js/63.4682e0f3.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmotto=this.webpackJsonpmotto||[]).push([[63],{158:function(t,n,e){"use strict";e.r(n),e.d(n,"default",(function(){return s}));e(2);var c=e(247),o=e(1);function s(){return Object(o.jsx)("div",{children:Object(o.jsx)(c.default,{})})}}}]);
+//# sourceMappingURL=63.4682e0f3.chunk.js.map
