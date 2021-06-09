dc3422ea3f6740986716c70e78ad157e86fb5960
venkatesh frontend <venkatesh.doodleblue@gmail.com>
2021-06-09 17:09:09 +0530

    removed bugs

--- src/component/AdminDashboard/AdminComponents/AdminTrainerSection/index.js
+++ src/component/AdminDashboard/AdminComponents/AdminTrainerSection/index.js
@@ -6,101 +6,120 @@ import { bindActionCreators } from "redux";
 import CommonPagination from "component/common/CommonPagination";
 
 const AdminTrainerSectionClass = (props) => {
-  const [trainerList, setTrainerList] = useState([]);
-  const [pageMetaData, setpageMetaData] = useState({});
+    const [trainerList, setTrainerList] = useState([]);
+    const [pageMetaData, setpageMetaData] = useState({});
 
-  useEffect(() => {
-    fetchAllTrainers(1);
-  }, []);
+    useEffect(() => {
+        fetchAllTrainers(1);
+    }, []);
 
-  function fetchAllTrainers(page = 1) {
-    props.getAllTrainerLists(page).then((data) => {
-      setTrainerList(data.list);
-      setpageMetaData(data.pageMetaData);
-    });
-  }
+    function fetchAllTrainers(page = 1) {
+        props.getAllTrainerLists(page).then((data) => {
+            setTrainerList(data.list);
+            setpageMetaData(data.pageMetaData);
+        });
+    }
 
-  function ChangeApproval(id, state) {
-    const data = { trainerStatus: state };
+    function ChangeApproval(id, state) {
+        const data = { trainerStatus: state };
 
-    props
-      .changeApproval(data, id)
-      .then(() => {
-        fetchAllTrainers();
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
+        props
+            .changeApproval(data, id)
+            .then(() => {
+                fetchAllTrainers();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }
 
-  return (
-    <div className="outter_container_AD">
-      <div className="container">
-        <h2>Admin Overall Data</h2>
-        <br></br>
-        <h3>Trainer List</h3>
-        <div className="admin">
-          <div className="tariner_info">
-            <div className="tariner_info_item_heading">Name</div>
-            <div className="tariner_info_item_heading">Email</div>
-            <div className="tariner_info_item_heading">Trainer Status</div>
-            <div className="tariner_info_item_heading">Action</div>
-          </div>
+    return (
+        <div className="outter_container_AD">
+            <div className="container">
+                <h2>Admin Overall Data</h2>
+                <br></br>
+                <h3>Trainer List</h3>
+                <div className="admin">
+                    <div className="tariner_info">
+                        <div className="tariner_info_item_heading">Name</div>
+                        <div className="tariner_info_item_heading">Email</div>
+                        <div className="tariner_info_item_heading">
+                            Trainer Status
+                        </div>
+                        <div className="tariner_info_item_heading">Action</div>
+                    </div>
 
-          {trainerList.map((item) => {
-            var approve;
-            if (item["trainerStatus"] === "approved") {
-              approve = (
-                <div onClick={() => ChangeApproval(item["id"], "disapproved")}>
-                  Disapprove
+                    {trainerList.map((item) => {
+                        var approve;
+                        if (item["trainerStatus"] === "approved") {
+                            approve = (
+                                <div
+                                    onClick={() =>
+                                        ChangeApproval(
+                                            item["id"],
+                                            "disapproved"
+                                        )
+                                    }
+                                >
+                                    Disapprove
+                                </div>
+                            );
+                        } else {
+                            approve = (
+                                <div
+                                    onClick={() =>
+                                        ChangeApproval(item["id"], "approved")
+                                    }
+                                >
+                                    Approve
+                                </div>
+                            );
+                        }
+
+                        return (
+                            <div className="trainer_info">
+                                <div className="tariner_info_item">
+                                    {item["firstName"]}
+                                </div>
+                                <div className="tariner_info_item">
+                                    {item["email"]}
+                                </div>
+                                <div className="tariner_info_item">
+                                    {item["trainerStatus"]}
+                                </div>
+                                <div className="tariner_info_item">
+                                    {" "}
+                                    <button className="tariner_info_button">
+                                        {approve}
+                                    </button>{" "}
+                                </div>
+                            </div>
+                        );
+                    })}
+                    <CommonPagination
+                        pageMetaData={pageMetaData}
+                        totalPosts={trainerList.length}
+                        pageChange={(e) => fetchAllTrainers(e)}
+                    />
                 </div>
-              );
-            } else {
-              approve = (
-                <div onClick={() => ChangeApproval(item["id"], "approved")}>
-                  Approve
-                </div>
-              );
-            }
-
-            return (
-              <div className="trainer_info">
-                <div className="tariner_info_item">{item["firstName"]}</div>
-                <div className="tariner_info_item">{item["email"]}</div>
-                <div className="tariner_info_item">{item["trainerStatus"]}</div>
-                <div className="tariner_info_item">
-                  {" "}
-                  <button className="tariner_info_button">
-                    {approve}
-                  </button>{" "}
-                </div>
-              </div>
-            );
-          })}
-          <CommonPagination
-            pageMetaData={pageMetaData}
-            totalPosts={trainerList.length}
-            pageChange={(e) => fetchAllTrainers(e)}
-          />
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators(
-    {
-      changeApproval,
-      getAllTrainerLists,
-    },
-    dispatch
-  );
+    return bindActionCreators(
+        {
+            changeApproval,
+            getAllTrainerLists,
+        },
+        dispatch
+    );
 };
 
 const AdminTrainerSection = connect(
-  null,
-  mapDispatchToProps
+    null,
+    mapDispatchToProps
 )(AdminTrainerSectionClass);
 
 export default AdminTrainerSection;
--- src/service/apiVariables.js
+++ src/service/apiVariables.js
@@ -1,259 +1,259 @@
 export const AuthApi = {
-  loginApi: {
-    api: "login",
-    method: "post",
-  },
-  userSignUp: {
-    api: "user/sign-up",
-    method: "post",
-  },
-  trainerSignUp: {
-    api: "trainer/sign-up",
-    method: "post",
-    baseUrl: "notis",
-  },
-  loginAdminApi: {
-    api: "login",
-    method: "post",
-  },
-  adminApi: {
-    api: "admin/login",
-    method: "post",
-  },
-  resetPassword: {
-    api: "user/reset-password",
-    method: "post",
-  },
-  changePasswordTrainer: {
-    api: "trainer/change-password",
-    method: "post",
-  },
-  changePasswordUser: {
-    api: "user/change-password",
-    method: "post",
-  },
+    loginApi: {
+        api: "login",
+        method: "post",
+    },
+    userSignUp: {
+        api: "user/sign-up",
+        method: "post",
+    },
+    trainerSignUp: {
+        api: "trainer/sign-up",
+        method: "post",
+        baseUrl: "notis",
+    },
+    loginAdminApi: {
+        api: "login",
+        method: "post",
+    },
+    adminApi: {
+        api: "admin/login",
+        method: "post",
+    },
+    resetPassword: {
+        api: "user/reset-password",
+        method: "post",
+    },
+    changePasswordTrainer: {
+        api: "trainer/change-password",
+        method: "post",
+    },
+    changePasswordUser: {
+        api: "user/change-password",
+        method: "post",
+    },
 };
 
 export const TrainerApi = {
-  trainerAvailableApi: {
-    url: "availableTrainer",
-    method: "get",
-    query: {
-      location: null,
-      date: null,
-      availability: null,
-      trainingType: null,
+    trainerAvailableApi: {
+        url: "availableTrainer",
+        method: "get",
+        query: {
+            location: null,
+            date: null,
+            availability: null,
+            trainingType: null,
+        },
+        get api() {
+            return (
+                this.url +
+                `?location=${this.query.location}&trainingType=["${this.query.trainingType}"]&date=["${this.query.date}","${this.query.date}"]&availability=["${this.query.availability}"]` +
+                `&timeZone=America/New_York`
+            );
+        },
+        set addQuery({ key, payload }) {
+            this.query[key] = payload;
+        },
     },
-    get api() {
-      return (
-        this.url +
-        `?location=${this.query.location}&trainingType=["${this.query.trainingType}"]&date=["${this.query.date}","${this.query.date}"]&availability=["${this.query.availability}"]` +
-        `&timeZone=America/New_York`
-      );
+    updateTrainerAvailabilityApi: {
+        api: "trainer",
+        method: "put",
     },
-    set addQuery({ key, payload }) {
-      this.query[key] = payload;
+    updateTrainerDetails: {
+        api: "trainer",
+        method: "put",
     },
-  },
-  updateTrainerAvailabilityApi: {
-    api: "trainer",
-    method: "put",
-  },
-  updateTrainerDetails: {
-    api: "trainer",
-    method: "put",
-  },
-  getTrainerApi: {
-    api: "trainer",
-    method: "get",
-  },
-  getTrainerSessionApi: {
-    url: "session/trainer",
-    method: "get",
-    iscalenderView: false,
-    baseURL: "session",
-    get api() {
-      return this.url;
+    getTrainerApi: {
+        api: "trainer",
+        method: "get",
     },
-    set addQuery({ key, payload }) {
-      this.query[key] = payload;
+    getTrainerSessionApi: {
+        url: "session/trainer",
+        method: "get",
+        iscalenderView: false,
+        baseURL: "session",
+        get api() {
+            return this.url;
+        },
+        set addQuery({ key, payload }) {
+            this.query[key] = payload;
+        },
     },
-  },
-  changeApproval: {
-    url: "trainer?trainerId=",
-    method: "put",
-    baseURL: "normal",
-    isAdmin: true,
-    id: null,
-    get api() {
-      return this.url + this.id;
+    changeApproval: {
+        url: "trainer?trainerId=",
+        method: "put",
+        baseURL: "normal",
+        isAdmin: true,
+        id: null,
+        get api() {
+            return this.url + this.id;
+        },
     },
-  },
-  getAllTrainerLists: {
-    url: "admin/trainers?limit=5&page=",
-    method: "get",
-    baseURL: "normal",
-    isAdmin: true,
-    page: 1,
-    get api() {
-      return this.url + this.page;
+    getAllTrainerLists: {
+        url: "admin/trainers?limit=5&page=",
+        method: "get",
+        baseURL: "normal",
+        isAdmin: true,
+        page: 1,
+        get api() {
+            return this.url + this.page;
+        },
     },
-  },
-  getStatsData: {
-    api: "get/stats",
-    method: "get",
-    baseURL: "normal",
-    isAdmin: true,
-  },
-  getAllUsersLists: {
-    url: "admin/users?limit=20&page=",
-    method: "get",
-    baseURL: "normal",
-    isAdmin: true,
-    page: 1,
-    get api() {
-      return this.url + this.page;
+    getStatsData: {
+        api: "get/stats",
+        method: "get",
+        baseURL: "normal",
+        isAdmin: true,
     },
-  },
-  fetchTrainersLists: {
-    url: "admin/trainers?limit=20&page=",
-    method: "get",
-    baseURL: "normal",
-    isAdmin: true,
-    page: 1,
-    get api() {
-      return this.url + this.page;
+    getAllUsersLists: {
+        url: "admin/users?limit=20&page=",
+        method: "get",
+        baseURL: "normal",
+        isAdmin: true,
+        page: 1,
+        get api() {
+            return this.url + this.page;
+        },
     },
-  },
-  getTrainerDetail: {
-    url: "trainer/id?trainerId=",
-    method: "get",
-    baseURL: "normal",
-    isAdmin: true,
-    id: null,
-    get api() {
-      return this.url + this.id;
+    fetchTrainersLists: {
+        url: "admin/trainers?limit=20&page=",
+        method: "get",
+        baseURL: "normal",
+        isAdmin: true,
+        page: 1,
+        get api() {
+            return this.url + this.page;
+        },
     },
-  },
-  trainerDetail: {
-    api: "trainer",
-    method: "get",
-    baseURL: "normal",
-  },
-  fileUpload: {
-    api: "upload/image",
-    method: "post",
-    baseURL: "normal",
-  },
-  getTrainerCalenderDetails: {
-    url: "trainer/calenderView?startDate=",
-    method: "get",
-    baseURL: "normal",
-    startDate: null,
-    endDate: null,
-    timeSlot: null,
-    get api() {
-      return (
-        this.url +
-        this.startDate +
-        "&endDate=" +
-        this.endDate +
-        "&timeBlock=" +
-        this.timeSlot +
-        `&timeZone=America/New_York`
-      );
+    getTrainerDetail: {
+        url: "trainer/id?trainerId=",
+        method: "get",
+        baseURL: "normal",
+        isAdmin: true,
+        id: null,
+        get api() {
+            return this.url + this.id;
+        },
     },
-  },
-  trainerSlot: {
-    api: "block/trainerSlot",
-    method: "put",
-    baseURL: "normal",
-  },
-  makeDefaultApi: {
-    api: "slot/make-default",
-    method: "post",
-    baseURL: "normal",
-  },
-  addTrainerSlotApi: {
-    api: "addTrainerSlot",
-    method: "post",
-    baseURL: "normal",
-  },
-  trainerChannel: {
-    api: "trainer/channel",
-    method: "get",
-    baseURL: "message",
-  },
-  trainerMyEarning: {
-    url: "payment-history/trainer?trainerId=",
-    method: "get",
-    baseURL: "payment",
-    trainerId: null,
-    get api() {
-      return this.url + this.trainerId;
+    trainerDetail: {
+        api: "trainer",
+        method: "get",
+        baseURL: "normal",
     },
-  },
+    fileUpload: {
+        api: "upload/image",
+        method: "post",
+        baseURL: "normal",
+    },
+    getTrainerCalenderDetails: {
+        url: "trainer/calenderView?startDate=",
+        method: "get",
+        baseURL: "normal",
+        startDate: null,
+        endDate: null,
+        timeSlot: null,
+        get api() {
+            return (
+                this.url +
+                this.startDate +
+                "&endDate=" +
+                this.endDate +
+                "&timeBlock=" +
+                this.timeSlot +
+                `&timeZone=America/New_York`
+            );
+        },
+    },
+    trainerSlot: {
+        api: "block/trainerSlot",
+        method: "put",
+        baseURL: "normal",
+    },
+    makeDefaultApi: {
+        api: "slot/make-default",
+        method: "post",
+        baseURL: "normal",
+    },
+    addTrainerSlotApi: {
+        api: "addTrainerSlot",
+        method: "post",
+        baseURL: "normal",
+    },
+    trainerChannel: {
+        api: "trainer/channel",
+        method: "get",
+        baseURL: "message",
+    },
+    trainerMyEarning: {
+        url: "payment-history/trainer?trainerId=",
+        method: "get",
+        baseURL: "payment",
+        trainerId: null,
+        get api() {
+            return this.url + this.trainerId;
+        },
+    },
 };
 
 export const PaymentApi = {
-  getStripeAccLink: {
-    api: "connected-account-link",
-    method: "post",
-    baseURL: "payment",
-  },
-  getPaymentMethods: {
-    api: "payment-methods",
-    method: "get",
-    baseURL: "payment",
-  },
+    getStripeAccLink: {
+        api: "connected-account-link",
+        method: "post",
+        baseURL: "payment",
+    },
+    getPaymentMethods: {
+        api: "payment-methods",
+        method: "get",
+        baseURL: "payment",
+    },
 };
 
 export const userApi = {
-  getUserDetail: {
-    api: "user",
-    method: "get",
-    baseURL: "normal",
-  },
-  userSession: {
-    api: "session/user",
-    method: "get",
-    baseURL: "session",
-  },
-  cancelSession: {
-    api: "session/update",
-    method: "put",
-    baseURL: "session",
-  },
-  scheduleSession: {
-    api: "session/schedule",
-    method: "post",
-    baseURL: "session",
-  },
-  getCalenderDetails: {
-    url: "trainer/calenderView?trainerId=",
-    method: "get",
-    baseURL: "normal",
-    trainerId: null,
-    startDate: null,
-    endDate: null,
-    timeSlot: null,
-    get api() {
-      return (
-        this.url +
-        this.trainerId +
-        "&startDate=" +
-        this.startDate +
-        "&endDate=" +
-        this.endDate +
-        "&timeBlock=" +
-        this.timeSlot +
-        `&timeZone=America/New_York`
-      );
+    getUserDetail: {
+        api: "user",
+        method: "get",
+        baseURL: "normal",
     },
-  },
-  editUserData: {
-    api: "user/edit",
-    method: "post",
-    baseURL: "normal",
-  },
+    userSession: {
+        api: "session/user",
+        method: "get",
+        baseURL: "session",
+    },
+    cancelSession: {
+        api: "session/update",
+        method: "put",
+        baseURL: "session",
+    },
+    scheduleSession: {
+        api: "session/schedule",
+        method: "post",
+        baseURL: "session",
+    },
+    getCalenderDetails: {
+        url: "trainer/calenderView?trainerId=",
+        method: "get",
+        baseURL: "normal",
+        trainerId: null,
+        startDate: null,
+        endDate: null,
+        timeSlot: null,
+        get api() {
+            return (
+                this.url +
+                this.trainerId +
+                "&startDate=" +
+                this.startDate +
+                "&endDate=" +
+                this.endDate +
+                "&timeBlock=" +
+                this.timeSlot +
+                `&timeZone=America/New_York`
+            );
+        },
+    },
+    editUserData: {
+        api: "user/edit",
+        method: "post",
+        baseURL: "normal",
+    },
 };
