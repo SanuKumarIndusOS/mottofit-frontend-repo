9e37e0dd5bdcc9e67a66aae737b1b83e17a9354e
Praveen M <praveen.02@doodleblue.com>
2021-06-24 00:57:28 +0530

    profile details

--- src/component/Home/NavbarHomePage/NavBarHomeMob.js
+++ src/component/Home/NavbarHomePage/NavBarHomeMob.js
@@ -4,7 +4,9 @@ import "./styles.scss";
 function NavBarHomeMob() {
     return (
         <div className="home_nav_mob">
-            
+            <div className="ham_but">ham</div>
+            <div className="logo_mob">Motto</div>
+            <div className="usr_icon_mob">user</div>
         </div>
     )
 }
