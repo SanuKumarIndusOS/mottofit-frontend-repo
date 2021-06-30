99422a4c30618cbe0cc1715ab637f7d85716fd4b
Praveen M <praveen.02@doodleblue.com>
2021-06-30 16:46:01 +0530

    trainer profile page

--- src/component/TrainerProfile/Profile/index.js
+++ src/component/TrainerProfile/Profile/index.js
@@ -637,7 +637,7 @@ const TrainerProfileClass = ({
 
       {/* Mobile Layout */}
 
-      <div className="trainer_profile_mob">
+      {/* <div className="trainer_profile_mob">
         <div className="heading">
           <div className="back_to_search">
             <p>Back to Search</p>
@@ -663,7 +663,7 @@ const TrainerProfileClass = ({
         <div className="scheduler"></div>
         <div className="book_button"></div>
         <div className="other_trainers"></div>
-      </div>
+      </div> */}
     </>
   );
 };
