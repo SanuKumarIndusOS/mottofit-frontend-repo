826b19da2bdfbf73b9d40765f530dd6d21387c5f
Praveen M <praveen.02@doodleblue.com>
2021-07-21 21:36:55 +0530

    245

--- src/pages/MobileChatBox/index.js
+++ src/pages/MobileChatBox/index.js
@@ -1,10 +1,12 @@
 
 import React from "react";
 import ChatBox from "component/common/Message/ChatBox";
+import { Link } from "react-router-dom";
 
 const ChatMobile = () => {
     return (
         <div>
+           {/* <Link to="">back</Link>  */}
             <ChatBox isDataPresent={1 > 0} />
         </div>
     );
