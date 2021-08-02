2c016619e46580cc41013eda88acb2a1808b6dc4
Praveen M <praveen.02@doodleblue.com>
2021-08-02 19:44:09 +0530

    notification url

--- src/config.js
+++ src/config.js
@@ -14,6 +14,7 @@ let config = {
     googleClientId:
       "667288998529-mltflfijefc7u5r8eaon8j8e1cmcfsot.apps.googleusercontent.com",
     facebookAppId: "310220110776502",
+    socketURL:"http://doodlebluelive.com:2355"
   },
   production: {
     api: {
@@ -22,12 +23,14 @@ let config = {
       sessionService: "https://apis.bookmotto.com/session/v1/",
       paymentService: "https://apis.bookmotto.com/payments/v1/",
       messgaeService: "https://apis.bookmotto.com/messaging/v1/",
+      notificationService:"https://apis.bookmotto.com/notification/v1/",
     },
     stripeUrl:
       "pk_live_51IJnd4BqgEC4bFYp4gC7EoInjpEKKjJXZMZGZurak1aK2ft1XN72QU355hRhLry3QmEucSKialeSBOGevqDJqfIJ00sU4zSO7O",
     googleClientId:
       "667288998529-mltflfijefc7u5r8eaon8j8e1cmcfsot.apps.googleusercontent.com",
     facebookAppId: "310220110776502",
+    socketURL:"https://apis.bookmotto.com/notification"
   },
 };
 
