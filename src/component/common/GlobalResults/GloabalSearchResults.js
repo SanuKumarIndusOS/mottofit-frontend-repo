31d422d0ec6f550c71448cbf8ee3c475b7dd4c5d
Praveen M <praveen.02@doodleblue.com>
2021-08-03 12:09:49 +0530

    row grid

--- src/component/common/GlobalResults/GloabalSearchResults.js
+++ src/component/common/GlobalResults/GloabalSearchResults.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./styles.scss";
 
 function GloabalSearchResults() {
-    return (
-        <div className="results_container">
-            <h1>Results</h1>
+  return (
+    <div className="results_container">
+      <h1>Results</h1>
+
+      <div className="results_grid">
+        <div className="row">
+            <div className="card"></div>
+      
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default GloabalSearchResults
+export default GloabalSearchResults;
