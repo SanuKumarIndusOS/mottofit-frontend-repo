b42ded164fb0f5750f1d0ec68cdda3ea77254152
Praveen M <praveen.02@doodleblue.com>
2021-08-04 20:15:43 +0530

    SearchFilter

--- src/component/common/SearchFilter/index.js
+++ src/component/common/SearchFilter/index.js
@@ -10,19 +10,31 @@ function index() {
 
       <div className="filter_type">
         <div className="filter_header">Availability</div>
-        <div className="filter_options"></div>
+        <div className="filter_options">
+          <div className="option_txt">Select a Time</div>
+          <div className="option_icon">&#10094;</div>
+        </div>
       </div>
       <div className="filter_type">
         <div className="filter_header">Schedule</div>
-        <div className="filter_options"></div>
+        <div className="filter_options">
+          <div className="option_txt">Select a Date</div>
+          <div className="option_icon">&#10094;</div>
+        </div>
       </div>
       <div className="filter_type">
         <div className="filter_header">Training Type</div>
-        <div className="filter_options"></div>
+        <div className="filter_options">
+          <div className="option_txt">Select a Category</div>
+          <div className="option_icon">&#10094;</div>
+        </div>
       </div>
       <div className="filter_type">
         <div className="filter_header">Location</div>
-        <div className="filter_options"></div>
+        <div className="filter_options">
+          <div className="option_txt">Select a Time</div>
+          <div className="option_icon"></div>
+        </div>
       </div>
     </div>
   );
