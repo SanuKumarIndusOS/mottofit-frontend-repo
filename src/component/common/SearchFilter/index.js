22d1c5018d9c20390fb698dd7d92a1bdb936f5e5
Praveen M <praveen.02@doodleblue.com>
2021-08-04 19:30:32 +0530

    SeachFilter 2

--- src/component/common/SearchFilter/index.js
+++ src/component/common/SearchFilter/index.js
@@ -8,10 +8,22 @@ function index() {
         <div className="circle"></div>
       </div>
 
-      <div className="filter_type"></div>
-      <div className="filter_type"></div>
-      <div className="filter_type"></div>
-      <div className="filter_type"></div>
+      <div className="filter_type">
+        <div className="filter_header">Availability</div>
+        <div className="filter_options"></div>
+      </div>
+      <div className="filter_type">
+        <div className="filter_header">Schedule</div>
+        <div className="filter_options"></div>
+      </div>
+      <div className="filter_type">
+        <div className="filter_header">Training Type</div>
+        <div className="filter_options"></div>
+      </div>
+      <div className="filter_type">
+        <div className="filter_header">Location</div>
+        <div className="filter_options"></div>
+      </div>
     </div>
   );
 }
