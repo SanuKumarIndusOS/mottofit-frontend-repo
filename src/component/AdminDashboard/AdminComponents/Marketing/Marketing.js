1aaa8f21ea020ed0cc68413f3a73acdcbd6dc55a
Praveen M <praveen.02@doodleblue.com>
2021-08-19 17:50:21 +0530

    incentives

--- src/component/AdminDashboard/AdminComponents/Marketing/Marketing.js
+++ src/component/AdminDashboard/AdminComponents/Marketing/Marketing.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import "./styles.scss";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import TextField from "@material-ui/core/TextField";
 
 function Marketing() {
-    return (
-        <div>
-            fdsdf
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (value) => {
+    setOpen(false);
+  };
+  return (
+    <div className="marketing_container">
+      <div className="marketing_header">
+        <h1>Incentives</h1>
+        <Button
+          variant="contained"
+          color="primary"
+          disableElevation
+          onClick={() => setOpen(true)}
+        >
+          <b>CREATE COUPON</b>
+        </Button>
+      </div>
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby="simple-dialog-title"
+        open={open}
+      >
+        <div className="create_coupon_form">
+          <h3>Create Coupon</h3>
+          <form>
+            <br></br>
+            <TextField label="CODE" variant="filled" />
+            <br></br>
+            <TextField label="TYPE" variant="filled" />
+            <br></br>
+            <TextField label="COUPON VALUE" variant="filled" />
+            <br></br>
+          </form>
         </div>
-    )
+      </Dialog>
+    </div>
+  );
 }
 
-export default Marketing
+export default Marketing;
