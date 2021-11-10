d7a9103b9ba489718a92e63a5c379baad38e0b2a
Praveen M <praveen.02@doodleblue.com>
2021-11-10 14:18:17 +0530

    profile Refactor

--- src/pages/Trainer/Profile/index.js
+++ src/pages/Trainer/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
- //import TrainerProfile from 'component/TrainerProfile/Profile/index';
-import TrainerProfile from 'component/TrainerProfileTwo/index';
+ import TrainerProfile from 'component/TrainerProfile/Profile/index';
+//import TrainerProfile from 'component/TrainerProfileTwo/index';
 
 export default function index() {
     return (
