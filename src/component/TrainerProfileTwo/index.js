b23aee49acbd63fd4b6e35265b300accd54a0ea0
Praveen M <praveen.02@doodleblue.com>
2021-11-30 03:33:30 +0530

    ProfileBody

--- src/component/TrainerProfileTwo/index.js
+++ src/component/TrainerProfileTwo/index.js
@@ -13,7 +13,9 @@ import { history } from "helpers";
 import { Link, useParams, useHistory } from "react-router-dom";
 
 import Quote from "assets/files/FindTrainer/Quote Icon.svg";
+import Tick from "assets/files/FindTrainer/Tick 1.svg";
 import Gallery from "react-grid-gallery";
+import BlueArrowButton from "component/common/BlueArrowButton";
 
 function TrainerProfileTwo({
   getTrainerDetail,
@@ -25,8 +27,6 @@ function TrainerProfileTwo({
 
   const [imageGrid, setImageGrid] = React.useState([]);
 
-
-
   React.useEffect(() => {
     getTrainerDetail(id, false).then((data) => {
       setTrainerData(data);
@@ -35,10 +35,8 @@ function TrainerProfileTwo({
         setImageGrid((oldArray) => [
           ...oldArray,
           {
-            src: item ,
+            src: item,
             thumbnail: item,
-            thumbnailWidth: 320,
-            
           },
         ]);
       });
@@ -127,9 +125,10 @@ function TrainerProfileTwo({
           <div className="trainer-pricing__scroll">
             View Leanna's Training options and rates
           </div>
+          <br></br>
           <div className="trainer-request-message">
             <div className="trainer-request-txt">Have a Question</div>
-            <div className="trainer-request-but">message Leanna</div>
+            <div className="trainer-request-but">message {trainerData?.firstName} <BlueArrowButton/></div>
           </div>
         </div>
         <div className="profile-body--right-pane">
@@ -138,7 +137,9 @@ function TrainerProfileTwo({
             <p>{trainerData?.myMotto}</p>
           </div>
           <div className="session-pricing">
-            <div className="right-pane--header">Train with Leanna</div>
+            <div className="right-pane--header">
+              Train with {trainerData?.firstName}
+            </div>
             <div className="right-pane--content">
               Browse & choose your session type to continue. You can then choose
               an available time or message your trainer for more availability &
@@ -156,16 +157,35 @@ function TrainerProfileTwo({
             />
           </div>
           <div className="trainer-about">
-            <div className="right-pane--header">About Jane</div>
+            <div className="right-pane--header">
+              About {trainerData?.firstName}
+            </div>
             <div className="right-pane--content">
               {trainerData?.trainingProcess}
             </div>
           </div>
           <div className="trainer-gallery">
-            {" "}
-            <Gallery images={imageGrid} enableImageSelection={false} maxRows={5} />
+            <Gallery
+              images={imageGrid}
+              enableImageSelection={false}
+              maxRows="5"
+            />
           </div>
-          <div className="certifications"></div>
+          <div className="certifications">
+            <div className="right-pane--header">Certifications</div>
+            <div className="certification-main">
+              {trainerData?.certification?.map((item) => {
+                return (
+                  <div className="certification-container">
+                    <div className="certification-icon">
+                      <img src={Tick} height="28" width="28"></img>
+                    </div>
+                    <div className="certification-name">{item?.certificate}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </div>
