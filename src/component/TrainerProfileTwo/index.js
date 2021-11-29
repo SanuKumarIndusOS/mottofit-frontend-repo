833706064ba4fdb2db65a27c8b74218e83f6c300
Praveen M <praveen.02@doodleblue.com>
2021-11-29 16:37:59 +0530

    new trainerPage

--- src/component/TrainerProfileTwo/index.js
+++ src/component/TrainerProfileTwo/index.js
@@ -66,7 +66,20 @@ function TrainerProfileTwo({
     // </div>
 
     <div className="trainer-profile-container">
-      <div className=""></div>
+      <div className="trainer-profile-header-container">
+        <div className="profile__black-area">
+        <div className="profile-picture"></div>
+          <div className="profile-info">
+            <div className="trainer-name">Leanna Brittis</div>
+            <div className="trainer-expertise">PILATES, STRENGTH & HIIT, YOGA, BOXING</div>
+          </div>
+          <div className="back-button">
+            <div className="back-button--symbol">&#8249;</div>
+            <div className="back-button--txt">Back to Results</div>
+          </div>
+        </div>
+       
+      </div>
     </div>
   );
 }
