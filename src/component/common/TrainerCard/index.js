1bc4c39f1f163d2df09a1cb9dda57ffe2b3b5b83
Praveen M <praveen.02@doodleblue.com>
2021-12-01 02:56:21 +0530

    Trainer Card

--- src/component/common/TrainerCard/index.js
+++ src/component/common/TrainerCard/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./styles.scss";
 
+import BlackCircleButton from "../../common/BlackCircleButton/ArrowHoverBlacked";
+
 function TrainerCard() {
     return (
         <div className="trainer-card-container">
-            
+            <div className="trainer-card--profile-picture"></div>
+            <div className="trainer-card--profile-brief"></div>
+            <div className="trainer-card--cta">
+                <div className="trainer-card--cta-action">BOOK A SESSION <BlackCircleButton/></div>
+                <div className="trainer-card--cta-price"><span>from </span>$125</div>
+            </div>
         </div>
     )
 }
