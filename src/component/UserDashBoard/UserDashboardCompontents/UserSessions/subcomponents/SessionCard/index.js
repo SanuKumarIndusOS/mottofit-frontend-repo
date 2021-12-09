71eda253b76b08082e95bd3e758938a827ddc2b6
Praveen M <praveen.02@doodleblue.com>
2021-12-09 17:42:55 +0530

    Session Card Ui

--- src/component/UserDashBoard/UserDashboardCompontents/UserSessions/subcomponents/SessionCard/index.js
+++ src/component/UserDashBoard/UserDashboardCompontents/UserSessions/subcomponents/SessionCard/index.js
@@ -6,20 +6,22 @@ function SessionCard() {
     <div className="session-card-container">
       <div className="session-info-container">
         <div className="session-date-box-container">
-          <div className="date--bg"></div>
-          <div className="month--bg"></div>
+          <div className="session-date-box">
+            <div className="date--bg">16</div>
+            <div className="month--bg">FEB</div>
+          </div>
         </div>
         <div className="session-info-content">
-          <div className="session__title"></div>
+          <div className="session__title">Motto Session with Heena</div>
           <div className="session__secondary-info"></div>
           <div className="session__controls"></div>
         </div>
       </div>
 
       <div className="package-info">
-          <div className="package-ribbon"></div>
-          <div className="package-remaining"></div>
-          <div className="package-validity"></div> 
+        <div className="package-ribbon"></div>
+        <div className="package-remaining"></div>
+        <div className="package-validity"></div>
       </div>
     </div>
   );
