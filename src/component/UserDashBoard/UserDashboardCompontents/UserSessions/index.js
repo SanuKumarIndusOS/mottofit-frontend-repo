1fa86620bfc971cfa1a318b59b12b8274195cc5c
Praveen M <praveen.02@doodleblue.com>
2021-12-09 17:13:10 +0530

    Session Card new

--- src/component/UserDashBoard/UserDashboardCompontents/UserSessions/index.js
+++ src/component/UserDashBoard/UserDashboardCompontents/UserSessions/index.js
@@ -1,20 +1,25 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 
 import TabControl from "./subcomponents/TabControl";
+import SessionCard from "./subcomponents/SessionCard";
 
 function UserSessions() {
   const [activeTab, setactiveTab] = useState("upcoming");
-  
+
   const handleChangeTab = (tabHeader) => setactiveTab(tabHeader);
-  
+
   return (
     <div className="user-sessions-container">
+      <div className="user-sessions__title">My Sessions</div>
       <div className="session-tabs">
         <TabControl activeTab={activeTab} handleChangeTab={handleChangeTab} />
       </div>
       <div className="session-tab-data">
-          
+        <div className="session-tab__header">{activeTab} sessions</div>
+        <div className="session-tab__content">
+           <SessionCard />
+        </div>
       </div>
     </div>
   );
---
+++ src/component/UserDashBoard/UserDashboardCompontents/UserSessions/subcomponents/SessionCard/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles.scss";
+
+function SessionCard() {
+  return (
+    <div className="session-card-container">
+      <div className="session-info-container">
+        <div className="session-date-box-container">
+          <div className="date--bg"></div>
+          <div className="month--bg"></div>
+        </div>
+        <div className="session-info-content">
+          <div className="session__title"></div>
+          <div className="session__secondary-info"></div>
+          <div className="session__controls"></div>
+        </div>
+      </div>
+
+      <div className="package-info">
+          <div className="package-ribbon"></div>
+          <div className="package-remaining"></div>
+          <div className="package-validity"></div> 
+      </div>
+    </div>
+  );
+}
+
+export default SessionCard;
