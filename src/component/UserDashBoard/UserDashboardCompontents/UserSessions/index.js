a10796898fbd95424507f12df05f78a962f8035f
Praveen M <praveen.02@doodleblue.com>
2021-12-09 16:18:01 +0530

    User Dashboard refactor

--- src/component/UserDashBoard/UserDashboardCompontents/UserSessions/index.js
+++ src/component/UserDashBoard/UserDashboardCompontents/UserSessions/index.js
@@ -5,13 +5,17 @@ import TabControl from "./subcomponents/TabControl";
 
 function UserSessions() {
   const [activeTab, setactiveTab] = useState("upcoming");
+  
   const handleChangeTab = (tabHeader) => setactiveTab(tabHeader);
+  
   return (
     <div className="user-sessions-container">
       <div className="session-tabs">
         <TabControl activeTab={activeTab} handleChangeTab={handleChangeTab} />
       </div>
-      <div className="session-tab-data"></div>
+      <div className="session-tab-data">
+          
+      </div>
     </div>
   );
 }
