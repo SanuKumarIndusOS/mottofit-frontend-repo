5b776175bafbf104b7c751e7ce36bc7160affa25
Praveen M <praveen.02@doodleblue.com>
2021-12-15 15:33:04 +0530

    ls Trainer Card

--- src/component/FindYourTrainer/subcomponents/BestMatch/index.js
+++ src/component/FindYourTrainer/subcomponents/BestMatch/index.js
@@ -1,16 +1,32 @@
 import React from "react";
-import './styles.scss';
+import "./styles.scss";
 
+// Component imports
 import TrainerCard from "component/common/TrainerCard/index";
+
+// TODO
+// 1. Seperate component for view more trainers/sessions button
 
 function BestMatch() {
   return (
     <div className="best-match-container">
-    
-    <div className=""></div>
-      <TrainerCard />
-      <TrainerCard />
-      <TrainerCard />
+      <div className="trainer-card__grid">
+        <TrainerCard />
+        <TrainerCard />
+        <TrainerCard />
+
+        <TrainerCard />
+        <TrainerCard />
+        <TrainerCard />
+
+        <TrainerCard />
+        <TrainerCard />
+        <TrainerCard />
+      </div>
+
+      <div className="view-more-trainer-container">
+        <div className="view-more-trainer__cta"> View More Trainers</div>
+      </div>
     </div>
   );
 }
